63794bd9af79786bdd7f25f2c63fd0a36dbc254b
igor-popravka <ipopravka@hotmail.com>
2018-09-04 00:12:36 +0300

    Install XDebuger

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,20 +5,26 @@
  * Date: 31.08.18
  * Time: 23:01
  */
-
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+class MainController {
 
-class MainController
-{
-    /**
-     * @Route("/")
-    */
-    public function routeMain(){
-        return new Response("All Ok!!");
-    }
+	/**
+	 * @Route("/phpinfo")
+	 */
+	public function routePHPInfo() {
+		echo phpinfo();
+		exit;
+	}
+
+	/**
+	 * @Route("/")
+	 */
+	public function routeMain() {
+		return new Response("Main Ok!!");
+	}
 
 }
